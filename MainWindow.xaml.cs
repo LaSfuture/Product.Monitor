7a4c9a465c061ae78d093840023595cc8bf42cf0
sunmc <sunmc@huarong.com>
2025-02-27 16:57:05 +0800

    UserControl增加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using ProductMonitor.ViewModels;
+using System.Threading.Tasks.Sources;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ProductMonitor
 {
@@ -19,6 +12,9 @@ namespace ProductMonitor
         public MainWindow()
         {
             InitializeComponent();
+
+            var mainWindowViewModel = new MainWindowViewModel();
+            this.DataContext = mainWindowViewModel;
         }
 
         private void Close_Button_Click(object sender, RoutedEventArgs e)
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,98 @@
+﻿using ProductMonitor.UserControls;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ProductMonitor.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private UserControl _monitorUC = new MonitorUC();
+        private string _timeStr = "";
+        private string _dateStr = "";
+        private string _weekStr = "";
+        private readonly DispatcherTimer _timer = new();
+        private readonly string[] _weekStrs = { "星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六" };
+
+        public MainWindowViewModel()
+        {
+            StartGetTime();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public UserControl MonitorUC
+        {
+            get => _monitorUC;
+            set
+            {
+                if (_monitorUC != value)
+                {
+                    _monitorUC = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MonitorUC)));
+                }
+            }
+        }
+
+        public string TimeStr
+        {
+            get => _timeStr;
+            set
+            {
+                if (_timeStr != value)
+                {
+                    _timeStr = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TimeStr)));
+                }
+            }
+        }
+
+        public string DateStr
+        {
+            get => _dateStr;
+            set
+            {
+                if (_dateStr != value)
+                {
+                    _dateStr = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateStr)));
+                }
+            }
+        }
+
+        public string WeekStr
+        {
+            get => _weekStr;
+            set
+            {
+                if (_weekStr != value)
+                {
+                    _weekStr = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(WeekStr)));
+                }
+            }
+        }
+
+        private void UpdateTime()
+        {
+            var curTime = DateTime.Now;
+            var weekStr = _weekStrs[(int)curTime.DayOfWeek];
+
+            TimeStr = curTime.ToString("HH:mm:ss");
+
+            if (WeekStr != weekStr)
+            {
+                DateStr = curTime.ToString("yyyy-MM-dd");
+                WeekStr = weekStr;
+            }
+        }
+
+        private void StartGetTime()
+        {
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (_, _) => UpdateTime();
+            _timer.Start();
+        }
+
+    }
+}
